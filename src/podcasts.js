9be47b4fb74e3427d6354b32628d33abe1d3a8ce
Erik Grinn <grinn.erik@gmail.com>
2025-02-03 15:15:45 -0500

    renamed icons to podcast-links

--- src/podcasts.js
+++ src/podcasts.js
@@ -11,39 +11,57 @@ podcasts.setAttribute("class", "podcasts");
 podcastsDiv.appendChild(podcasts)
 
 class Podcast {
-  constructor(host, podcastIcon, description, links) {
-    this.host = host;
+  constructor(title, podcastIcon, description, links) {
+    this.title = title;
     this.podcastIcon = podcastIcon;
     this.description = description;
     this.links = links;
 
+    // create podcast container
     const podcast = document.createElement("div");
     podcast.setAttribute("class", "podcast");
 
+    // create and append podcast title
     const podcastTitle = document.createElement("div");
     podcastTitle.setAttribute("class", "podcast-title");
-    podcastTitle.textContent = this.host;
+    podcastTitle.textContent = this.title;
     podcast.appendChild(podcastTitle);
 
-    // content div container for image and description
+    // create content container for image and description
     const podcastContent = document.createElement("div");
     podcastContent.setAttribute("class", "podcast-content");
 
-    // image
-    const podcastIconImg = document.createElement("img");
-    podcastIconImg.setAttribute("src", this.podcastIcon);
-    podcastIconImg.setAttribute("alt", `${this.host} icon`);
-    podcastContent.appendChild(podcastIconImg);
+        // create and append image to podcast-content container
+        const podcastIconImg = document.createElement("img");
+        podcastIconImg.setAttribute("src", this.podcastIcon);
+        podcastIconImg.setAttribute("alt", `${this.host} icon`);
+        podcastContent.appendChild(podcastIconImg);
 
-    // description
-    const podcastDescription = document.createElement("div");
-    podcastDescription.setAttribute("class", "podcast-description");
-    podcastDescription.textContent = this.description;
-    podcastContent.appendChild(podcastDescription);
+        // create and append image to podcast-content container
+        const podcastDescription = document.createElement("div");
+        podcastDescription.setAttribute("class", "podcast-description");
+        podcastDescription.textContent = this.description;
+        podcastContent.appendChild(podcastDescription);
 
-    // append contentDiv to podcastDiv
+    // append podcast-content to podcast
     podcast.appendChild(podcastContent);
 
+    // create icon container for links
+
+
+    // // Create and append podcast links
+    // const podcastLinks = document.createElement('div');
+    // podcastLinks.setAttribute('class', 'podcast-links');
+    // this.links.forEach(link => {
+    //     const linkElement = document.createElement('a');
+    //     linkElement.setAttribute('href', link);
+    //     linkElement.setAttribute('target', '_blank');
+    //     linkElement.textContent = link;
+    //     podcastLinks.appendChild(linkElement);
+    // });
+    // podcast.appendChild(podcastLinks);
+
+    // finally, append podcast to podcasts
     podcasts.appendChild(podcast);
   }
 }
@@ -91,7 +109,7 @@ const podcast3 = new Podcast(
 //                 <img src='./files/images/podcast-icons/jesse-michels.jpeg'>
 //                     The Jesse Michels Podcast is a thought-provoking show that explores unconventional ideas, emerging technologies, and deep dives into history, science, and culture.
 //                 </div>
-//                 <div class="icons">
+//                 <div class="podcast-links">
 //                     <a href="https://www.youtube.com/Jessemichels" target="_blank" rel="noopener noreferrer"><img src="./files/images/icons/youtube.svg" heigh="25px" width="25px"></a>
 //                     <a href="https://open.spotify.com/show/1WqA0m5xTcp4n6qmCQfS2K" target="_blank" rel="noopener noreferrer"><img src="./files/images/icons/spotify.svg" heigh="25px" width="25px"></a>
 //                     <a href="https://podcasts.apple.com/my/podcast/american-alchemy/id1771917740" target="_blank" rel="noopener noreferrer"><img src="./files/images/icons/apple.svg" heigh="25px" width="25px"></a>
