34f9726c94d8435444f597218210c7520d57d0d8
Erik Grinn <grinn.erik@gmail.com>
2025-02-03 15:39:21 -0500

    links working

--- src/podcasts.js
+++ src/podcasts.js
@@ -46,25 +46,48 @@ class Podcast {
     // append podcast-content to podcast
     podcast.appendChild(podcastContent);
 
-    // create icon container for links
-
-
     // // Create and append podcast links
-    // const podcastLinks = document.createElement('div');
-    // podcastLinks.setAttribute('class', 'podcast-links');
-    // this.links.forEach(link => {
-    //     const linkElement = document.createElement('a');
-    //     linkElement.setAttribute('href', link);
-    //     linkElement.setAttribute('target', '_blank');
-    //     linkElement.textContent = link;
-    //     podcastLinks.appendChild(linkElement);
-    // });
-    // podcast.appendChild(podcastLinks);
+    const podcastLinks = document.createElement('div');
+    podcastLinks.setAttribute('class', 'podcast-links');
+    this.links.forEach((link, index) => {
+        const linkElement = document.createElement('a');
+        linkElement.setAttribute('href', link);
+        linkElement.setAttribute('target', '_blank');
+    
+        // Set different innerHTML based on the index
+        if (index === 0) {
+            linkElement.innerHTML = `<img src="./files/images/link-icons/youtube.svg" height="25px" width="25px" alt="YouTube">`;
+        } else if (index === 1) {
+            linkElement.innerHTML = `<img src="./files/images/link-icons/spotify.svg" height="25px" width="25px" alt="Spotify">`;
+        } else if (index === 2) {
+            linkElement.innerHTML = `<img src="./files/images/link-icons/apple.svg" height="25px" width="25px" alt="Apple Podcast">`;
+        }
+        podcastLinks.appendChild(linkElement);
+    })
+    podcast.appendChild(podcastLinks);
 
     // finally, append podcast to podcasts
     podcasts.appendChild(podcast);
   }
 }
+
+// podcastsDiv.innerHTML = `
+//         <div class="podcasts-header"><b>Podcasts</b></div>
+//         <div class="podcasts">
+//             <div class="podcast">
+//                 <div class="podcast-title">Jesse Michels</div>
+//                 <div class="podcast-content">
+//                 <img src='./files/images/podcast-icons/jesse-michels.jpeg'>
+//                     The Jesse Michels Podcast is a thought-provoking show that explores unconventional ideas, emerging technologies, and deep dives into history, science, and culture.
+//                 </div>
+//                 <div class="podcast-links">
+//                     <a href="https://www.youtube.com/Jessemichels" target="_blank" rel="noopener noreferrer"><img src="./files/images/icons/youtube.svg" heigh="25px" width="25px"></a>
+//                     <a href="https://open.spotify.com/show/1WqA0m5xTcp4n6qmCQfS2K" target="_blank" rel="noopener noreferrer"><img src="./files/images/icons/spotify.svg" heigh="25px" width="25px"></a>
+//                     <a href="https://podcasts.apple.com/my/podcast/american-alchemy/id1771917740" target="_blank" rel="noopener noreferrer"><img src="./files/images/icons/apple.svg" heigh="25px" width="25px"></a>
+//                 </div>
+//             </div>
+//         </div>
+// `;
 
 const podcast1 = new Podcast(
   "Jesse Michels",
@@ -98,24 +121,5 @@ const podcast3 = new Podcast(
       "https://podcasts.apple.com/my/podcast/american-alchemy/id1771917740",
     ]
   );
-
-
-// podcastsDiv.innerHTML = `
-//         <div class="podcasts-header"><b>Podcasts</b></div>
-//         <div class="podcasts">
-//             <div class="podcast">
-//                 <div class="podcast-title">Jesse Michels</div>
-//                 <div class="podcast-content">
-//                 <img src='./files/images/podcast-icons/jesse-michels.jpeg'>
-//                     The Jesse Michels Podcast is a thought-provoking show that explores unconventional ideas, emerging technologies, and deep dives into history, science, and culture.
-//                 </div>
-//                 <div class="podcast-links">
-//                     <a href="https://www.youtube.com/Jessemichels" target="_blank" rel="noopener noreferrer"><img src="./files/images/icons/youtube.svg" heigh="25px" width="25px"></a>
-//                     <a href="https://open.spotify.com/show/1WqA0m5xTcp4n6qmCQfS2K" target="_blank" rel="noopener noreferrer"><img src="./files/images/icons/spotify.svg" heigh="25px" width="25px"></a>
-//                     <a href="https://podcasts.apple.com/my/podcast/american-alchemy/id1771917740" target="_blank" rel="noopener noreferrer"><img src="./files/images/icons/apple.svg" heigh="25px" width="25px"></a>
-//                 </div>
-//             </div>
-//         </div>
-// `;
 
 export { podcastsDiv };
