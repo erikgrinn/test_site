4b43ba394d3fb3351b5e52833443a8ba2ad5200c
Erik Grinn <grinn.erik@gmail.com>
2025-02-02 23:02:45 -0500

    image, styling adjustments

--- src/podcasts.js
+++ src/podcasts.js
@@ -1,10 +1,24 @@
 const podcastsDiv = document.createElement("div");
 podcastsDiv.innerHTML = `
-        <div class="projects-header"><b>Podcasts</b></div>
-        <div class="projects">
-            <div class="project">
-                <div class="project-title">Calculator</div>
-                <div class="project-content">
+        <div class="podcasts-header"><b>Podcasts</b></div>
+        <div class="podcasts">
+            <div class="podcast">
+                <div class="podcast-title">Jesse Michels</div>
+                <div class="podcast-content">
+                <img src='./files/images/jesse-michels.jpeg'>
+                    The Jesse Michels Podcast is a thought-provoking show that explores unconventional ideas, emerging technologies, and deep dives into history, science, and culture. 
+                    Hosted by Jesse Michels, the podcast features in-depth interviews with experts, entrepreneurs, and intellectuals, often covering topics like AI, biotech, geopolitics, 
+                    and hidden narratives. 
+                </div>
+                <div class="icons">
+                    <a href="https://github.com/erikgrinn/odin-calculator" target="_blank" rel="noopener noreferrer"><img src="./icons/star-plus-outline.svg" heigh="25px" width="25px"></a>
+                    <a href="https://erikgrinn.github.io/odin-calculator/" target="_blank" rel="noopener noreferrer"><img src="./icons/eye-plus-outline.svg" heigh="25px" width="25px"></a>
+                    <a href="https://github.com/erikgrinn" target="_blank" rel="noopener noreferrer"><img src="./icons/share-variant-outline.svg" heigh="25px" width="25px"></a>
+                </div>
+            </div>
+            <div class="podcast">
+                <div class="podcast-title">Calculator-2</div>
+                <div class="podcast-content">
                     Are you tired of doing every single calculation by hand?
                     Does your abacus display signs of extended wear and tear?
                     This calculator app is here to solve all of your calculation woes. 
@@ -16,23 +30,9 @@ podcastsDiv.innerHTML = `
                     <a href="https://github.com/erikgrinn" target="_blank" rel="noopener noreferrer"><img src="./icons/share-variant-outline.svg" heigh="25px" width="25px"></a>
                 </div>
             </div>
-            <div class="project">
-                <div class="project-title">Calculator-2</div>
-                <div class="project-content">
-                    Are you tired of doing every single calculation by hand?
-                    Does your abacus display signs of extended wear and tear?
-                    This calculator app is here to solve all of your calculation woes. 
-                    Just don't perform the forbidden operation...
-                </div>
-                <div class="icons">
-                    <a href="https://github.com/erikgrinn/odin-calculator" target="_blank" rel="noopener noreferrer"><img src="./icons/star-plus-outline.svg" heigh="25px" width="25px"></a>
-                    <a href="https://erikgrinn.github.io/odin-calculator/" target="_blank" rel="noopener noreferrer"><img src="./icons/eye-plus-outline.svg" heigh="25px" width="25px"></a>
-                    <a href="https://github.com/erikgrinn" target="_blank" rel="noopener noreferrer"><img src="./icons/share-variant-outline.svg" heigh="25px" width="25px"></a>
-                </div>
-            </div>
-            <div class="project">
-                <div class="project-title">Calculator-3</div>
-                <div class="project-content">
+            <div class="podcast">
+                <div class="podcast-title">Calculator-3</div>
+                <div class="podcast-content">
                     Are you tired of doing every single calculation by hand?
                     Does your abacus display signs of extended wear and tear?
                     This calculator app is here to solve all of your calculation woes. 
