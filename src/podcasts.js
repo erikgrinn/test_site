275066b5906614fe633b265c6763a2b103bcca92
Erik Grinn <grinn.erik@gmail.com>
2025-02-02 22:33:07 -0500

    removed extraneous main div

--- src/podcasts.js
+++ src/podcasts.js
@@ -1,53 +1,51 @@
-const podcastsDiv = document.createElement('div')
+const podcastsDiv = document.createElement("div");
 podcastsDiv.innerHTML = `
-        <div class="main">
-            <div class="projects-header"><b>Podcasts</b></div>
-            <div class="projects">
-                <div class="project">
-                    <div class="project-title">Calculator</div>
-                    <div class="content">
-                        Are you tired of doing every single calculation by hand?
-                        Does your abacus display signs of extended wear and tear?
-                        This calculator app is here to solve all of your calculation woes. 
-                        Just don't perform the forbidden operation...
-                    </div>
-                    <div class="icons">
-                        <a href="https://github.com/erikgrinn/odin-calculator" target="_blank" rel="noopener noreferrer"><img src="./icons/star-plus-outline.svg" heigh="25px" width="25px"></a>
-                        <a href="https://erikgrinn.github.io/odin-calculator/" target="_blank" rel="noopener noreferrer"><img src="./icons/eye-plus-outline.svg" heigh="25px" width="25px"></a>
-                        <a href="https://github.com/erikgrinn" target="_blank" rel="noopener noreferrer"><img src="./icons/share-variant-outline.svg" heigh="25px" width="25px"></a>
-                    </div>
+        <div class="projects-header"><b>Podcasts</b></div>
+        <div class="projects">
+            <div class="project">
+                <div class="project-title">Calculator</div>
+                <div class="content">
+                    Are you tired of doing every single calculation by hand?
+                    Does your abacus display signs of extended wear and tear?
+                    This calculator app is here to solve all of your calculation woes. 
+                    Just don't perform the forbidden operation...
                 </div>
-                <div class="project">
-                    <div class="project-title">Calculator-2</div>
-                    <div class="content">
-                        Are you tired of doing every single calculation by hand?
-                        Does your abacus display signs of extended wear and tear?
-                        This calculator app is here to solve all of your calculation woes. 
-                        Just don't perform the forbidden operation...
-                    </div>
-                    <div class="icons">
-                        <a href="https://github.com/erikgrinn/odin-calculator" target="_blank" rel="noopener noreferrer"><img src="./icons/star-plus-outline.svg" heigh="25px" width="25px"></a>
-                        <a href="https://erikgrinn.github.io/odin-calculator/" target="_blank" rel="noopener noreferrer"><img src="./icons/eye-plus-outline.svg" heigh="25px" width="25px"></a>
-                        <a href="https://github.com/erikgrinn" target="_blank" rel="noopener noreferrer"><img src="./icons/share-variant-outline.svg" heigh="25px" width="25px"></a>
-                    </div>
+                <div class="icons">
+                    <a href="https://github.com/erikgrinn/odin-calculator" target="_blank" rel="noopener noreferrer"><img src="./icons/star-plus-outline.svg" heigh="25px" width="25px"></a>
+                    <a href="https://erikgrinn.github.io/odin-calculator/" target="_blank" rel="noopener noreferrer"><img src="./icons/eye-plus-outline.svg" heigh="25px" width="25px"></a>
+                    <a href="https://github.com/erikgrinn" target="_blank" rel="noopener noreferrer"><img src="./icons/share-variant-outline.svg" heigh="25px" width="25px"></a>
                 </div>
-                <div class="project">
-                    <div class="project-title">Calculator-3</div>
-                    <div class="content">
-                        Are you tired of doing every single calculation by hand?
-                        Does your abacus display signs of extended wear and tear?
-                        This calculator app is here to solve all of your calculation woes. 
-                        Just don't perform the forbidden operation...
-                    </div>
-                    <div class="icons">
-                        <a href="https://github.com/erikgrinn/odin-calculator" target="_blank" rel="noopener noreferrer"><img src="./icons/star-plus-outline.svg" heigh="25px" width="25px"></a>
-                        <a href="https://erikgrinn.github.io/odin-calculator/" target="_blank" rel="noopener noreferrer"><img src="./icons/eye-plus-outline.svg" heigh="25px" width="25px"></a>
-                        <a href="https://github.com/erikgrinn" target="_blank" rel="noopener noreferrer"><img src="./icons/share-variant-outline.svg" heigh="25px" width="25px"></a>
-                    </div>
+            </div>
+            <div class="project">
+                <div class="project-title">Calculator-2</div>
+                <div class="content">
+                    Are you tired of doing every single calculation by hand?
+                    Does your abacus display signs of extended wear and tear?
+                    This calculator app is here to solve all of your calculation woes. 
+                    Just don't perform the forbidden operation...
+                </div>
+                <div class="icons">
+                    <a href="https://github.com/erikgrinn/odin-calculator" target="_blank" rel="noopener noreferrer"><img src="./icons/star-plus-outline.svg" heigh="25px" width="25px"></a>
+                    <a href="https://erikgrinn.github.io/odin-calculator/" target="_blank" rel="noopener noreferrer"><img src="./icons/eye-plus-outline.svg" heigh="25px" width="25px"></a>
+                    <a href="https://github.com/erikgrinn" target="_blank" rel="noopener noreferrer"><img src="./icons/share-variant-outline.svg" heigh="25px" width="25px"></a>
+                </div>
+            </div>
+            <div class="project">
+                <div class="project-title">Calculator-3</div>
+                <div class="content">
+                    Are you tired of doing every single calculation by hand?
+                    Does your abacus display signs of extended wear and tear?
+                    This calculator app is here to solve all of your calculation woes. 
+                    Just don't perform the forbidden operation...
+                </div>
+                <div class="icons">
+                    <a href="https://github.com/erikgrinn/odin-calculator" target="_blank" rel="noopener noreferrer"><img src="./icons/star-plus-outline.svg" heigh="25px" width="25px"></a>
+                    <a href="https://erikgrinn.github.io/odin-calculator/" target="_blank" rel="noopener noreferrer"><img src="./icons/eye-plus-outline.svg" heigh="25px" width="25px"></a>
+                    <a href="https://github.com/erikgrinn" target="_blank" rel="noopener noreferrer"><img src="./icons/share-variant-outline.svg" heigh="25px" width="25px"></a>
                 </div>
             </div>
         </div>
-`
+`;
 // podcastsDiv.innerHTML = `
 // Jesse Michels
 // <br>
@@ -59,4 +57,4 @@ podcastsDiv.innerHTML = `
 // <br>
 // Wartime Stories`
 
-export {podcastsDiv}
+export { podcastsDiv };
