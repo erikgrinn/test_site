0b09650210f2e38766bbc277f61488fccb572c09
Erik Grinn <grinn.erik@gmail.com>
2025-02-02 22:11:02 -0500

    good cards, but issue with title spacing?

--- src/podcasts.js
+++ src/podcasts.js
@@ -31,6 +31,20 @@ podcastsDiv.innerHTML = `
                         <a href="https://github.com/erikgrinn" target="_blank" rel="noopener noreferrer"><img src="./icons/share-variant-outline.svg" heigh="25px" width="25px"></a>
                     </div>
                 </div>
+                <div class="project">
+                    <div class="project-title">Calculator-3</div>
+                    <div class="content">
+                        Are you tired of doing every single calculation by hand?
+                        Does your abacus display signs of extended wear and tear?
+                        This calculator app is here to solve all of your calculation woes. 
+                        Just don't perform the forbidden operation...
+                    </div>
+                    <div class="icons">
+                        <a href="https://github.com/erikgrinn/odin-calculator" target="_blank" rel="noopener noreferrer"><img src="./icons/star-plus-outline.svg" heigh="25px" width="25px"></a>
+                        <a href="https://erikgrinn.github.io/odin-calculator/" target="_blank" rel="noopener noreferrer"><img src="./icons/eye-plus-outline.svg" heigh="25px" width="25px"></a>
+                        <a href="https://github.com/erikgrinn" target="_blank" rel="noopener noreferrer"><img src="./icons/share-variant-outline.svg" heigh="25px" width="25px"></a>
+                    </div>
+                </div>
             </div>
         </div>
 `
